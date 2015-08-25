99e92951f1e54535b9ba9b0ff83287a3263ff4e5
dougtebay <dougtebay@gmail.com>
2015-08-25 13:01:19 -0400

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+  i = 0
+  x = []
+  while i < collection.length
+    y = collection[i]
+    if yield(y)
+      x << collection[i]
+    end
+    i += 1
+  end
+  x
 end
+
+nums = [1, 2, 3, 4, 5]
+my_select(nums) { |y| y.odd? }
