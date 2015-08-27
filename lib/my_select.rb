20f5a400a6995384366f8ea2b3d2a28b77650710
ohminimarie <mcjalcantara@gmail.com>
2015-08-27 00:37:26 -0700

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
-def my_select(collection)
+def my_select(nums)
  # your code here!
+  i = 0
+  thing = []
+    while i < nums.length
+    thing.push(nums[i]) if yield(nums[i]) == true
+      i += 1
+    end
+  thing
 end
