3ca78e75f643f060a2d809a044d000f6e6e80340
eenymini <hhl262@nyu.edu>
2015-09-11 20:59:40 -0400

    done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- # your code here!
+ arraylength = 0
+ my_array = []
+while arraylength < collection.length
+  i = collection[arraylength]
+  my_array << collection[arraylength] if yield (i)
+  arraylength += 1
+end
+return my_array
 end
