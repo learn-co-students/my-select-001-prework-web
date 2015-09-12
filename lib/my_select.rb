ac60b754e38363b60e68977c4e8ec6b1267d8eb4
pedrotp <pedro@pedrotp.com>
2015-09-11 20:16:11 -0400

    yieeeeld

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  index = 0
+  output = []
+  while index < collection.length
+    boolean = yield(collection[index])
+    if boolean == true
+      output.push(collection[index])
+    end
+    index += 1
+  end
+  output
 end
