10133da6e9583650dbea535619433656a393876f
matthewmain <matthewwmain@gmail.com>
2015-09-15 09:02:19 -0400

    wrote "my_select" method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
+    i = 0
+    sel_arr = []
+    while i < collection.size
+        sel_arr << collection[i] if yield(collection[i]) == true
+        i += 1
+    end
+    sel_arr
 end
