0e56a340683711e6048580ca0f4a004be51ff50f
moziff <matteoziff@gmail.com>
2015-09-16 14:34:50 -0400

    my select is all done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+    new_array=[]
+    num = 0
+    while num < collection.length
+        if yield(collection[num])== true
+            new_array.push(collection[num])
+            num+=1
+        else
+            num+=1
+        end
+    end
+    new_array
 end
