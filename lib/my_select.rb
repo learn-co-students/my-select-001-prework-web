8c032457b174ede72f0ed296e5c908421f4bf19b
skipsuva <skip.suva@gmail.com>
2015-09-20 15:42:50 -0400

    built a select method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,35 @@
+
 def my_select(collection)
- # your code here!
+  idx = 0
+  array = []
+
+  while idx < collection.length
+
+    if collection[idx].even?
+      yield(collection[idx])
+      array << (collection[idx])
+      idx += 1
+
+    else
+      idx += 1
+
+    end
+  end
+  array
 end
+
+
+
+
+
+
+def my_collect(values)
+  languages = []
+  idx = 0
+  while idx < values.length
+    languages << yield(values[idx])
+    idx += 1
+  end
+  languages
+end
+
