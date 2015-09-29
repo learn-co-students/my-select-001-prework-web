9c3a69cca74c565a2eb84766d252b1fb6bae4f3c
aleksa000777 <aleksa000777@mail.ru>
2015-09-29 18:04:32 -0400

    done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
+  newarr = []
+  i=0
+  while i<collection.length
+    if yield(collection[i])==true
+      newarr << collection[i]
+    end
+    i+=1
+  end
+  newarr
  # your code here!
 end
