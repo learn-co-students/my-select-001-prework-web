334e61b92db9452ed651891b9cb5bae40809d5e7
jmona789 <jmona789@gmail.com>
2015-10-03 19:33:58 -0400

    Done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+new_array = []
+num = 0
+  collection.size.times do 
+    if yield(collection[num]) == true
+      new_array << collection[num]
+      num += 1
+    else 
+      num += 1
+  end
+  end
+  return new_array
 end
