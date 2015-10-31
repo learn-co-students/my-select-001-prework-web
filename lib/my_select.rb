b1d4a63129b69ab4b4dbf37b9400134008cd056a
KhalilC <klcrumpler@gmail.com>
2015-10-31 03:47:52 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
  # your code here!
+  return collection unless block_given?
+  new_array = []
+  for element in collection
+    new_array << element if yield(element)
+  end
+  new_array 
 end
