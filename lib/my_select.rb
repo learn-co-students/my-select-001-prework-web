ba0116b6657c6b8df8b62ab7e139c23087287ec9
db1641 <damian.bidikov@gmail.com>
2015-11-05 15:15:54 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  selection = []
+  i = 0
+  while i < array.length
+    if yield(array[i]) == true
+      selection << array[i]
+    end
+    i +=1 
+  end
+  selection
 end
+
+
+
+my_select([2, 3, 4, 5, 6]) do |number|
+  number.odd?
+end 
