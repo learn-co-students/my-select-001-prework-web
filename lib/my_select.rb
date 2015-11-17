e93b06be3bc5c5223ed09248be5cd28e2795fa66
psswf1172 <jennifermabella@gmail.com>
2015-11-17 18:20:22 -0500

    my-select done

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  count = 0
+  if collection.empty?
+    puts "This block should not run!"
+  else
+    while count < collection.length
+      count += 1
+      new_collection = collection.find_all {|n| n%2 == 0}
+    end
+  end
+  return new_collection
 end
