fdfc1b9a17367a28a69cf64dbc22bbd3f8c556df
Aemrox <adam.may2164@gmail.com>
2015-11-25 23:24:57 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ i=0
+ returnary=[]
+ until i >= collection.length do 
+    if (yield collection[i])
+      returnary << collection[i]
+    end
+   i += 1
+ end
+ returnary
 end
