fa6250c6fa00a9458b4dc66a288b01195b7f0496
febbraiod <donedesign1@gmail.com>
2015-11-28 14:41:09 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  new_collection = []
+
+  i = 0
+  while i < collection.length
+    j = yield collection[i]
+    if j == true
+      new_collection << collection[i]
+    end
+    i += 1
+  end
+
+  new_collection
 end
