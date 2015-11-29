d91c5df6c7eda8a88548ca276d2c6836dc62ba25
zdg2102 <zacharydgarcia@gmail.com>
2015-11-29 19:18:19 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
 def my_select(collection)
  # your code here!
+  i = 0
+  output_arr = []
+  while i < collection.size
+    check_bool = yield(collection[i])
+    if check_bool == true
+      output_arr << collection[i]
+    end
+    i += 1
+  end
+  output_arr
+end
+
+collection = [1, 2, 3, 4, 5]
+my_select(collection) do |num|
+  num.even?
 end
