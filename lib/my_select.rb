04001f63a1ad8b90b302cbbfe4a8cae14176e21a
retraido <enzosarin@gmail.com>
2015-12-05 18:52:34 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
-end
+    new_collection = []
+  counter = 0
+  while counter < collection.length
+  if yield(collection[counter]) == true
+      new_collection << collection[counter]
+  counter +=1
+  else counter += 1
+        end
+    end
+    new_collection
+    end
+    
+            
+            collection = [1,2,3,4,5]
+            
+   my_select(collection) do |check|
+       check.even?
+        end
