2c2162436ef8fd7d740cb6e6d9c47a55134cd941
devinburnette <devin.burnette@gmail.com>
2015-12-07 22:47:47 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+  i = 0
+  evens = []
+  while i < collection.length
+    if yield(collection[i])
+      evens.push(collection[i])
+    end
+    i += 1
+  end
+  evens
+end
+
+collection = [1,2,3,4]
+my_select(collection) do |x|
+    x.even?
 end
