23e5077f561b7d92641667ecc744e8ff65f07862
RodAquino <rodrigo_59@hotmail.com>
2015-12-10 22:43:07 +0000

    nitrous-backup at Thu Dec 10 22:43:06 UTC 2015

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,14 @@
 collection = []
+new_collection = []
 def my_select(collection)
   new_collection = []
   collection.each do |x|
     if x.even? == true
-    new_collection << x
+      yield(x)
+    #new_collection << x
   end
 end
 return new_collection
 end
-
+my_select{|x| new_collection << x}
 
