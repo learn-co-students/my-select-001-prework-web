100d5e9de3060ff33efbf365171126f82bfebe73
sofiajeanne <sofia.rogg@gmail.com>
2015-12-14 16:23:36 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    i = 0
+    true_array = []
+    while i < collection.size
+      if yield(collection[i]) == true
+        true_array << collection[i]
+        puts collection[i]
+      end
+      i += 1
+    end
+  else
+    puts "No block was given!"
+  end
+  true_array
+end
+
+collection = [1, 2, 3, 4]
+my_select(collection) do |num|
+  num.even? == true
 end
