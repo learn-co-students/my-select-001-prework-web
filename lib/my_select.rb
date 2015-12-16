ccd3f6ee42e270db2bc438d4d72c0ffb67ac6fd9
Blake Johnston <johnstonbl01@gmail.com>
2015-12-15 18:13:17 -0600

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
-def my_select(collection)
- # your code here!
+def my_select (collection)
+   new_array = []
+   collection.each do |element|
+      if yield(element)
+         new_array.push(element)
+      end
+   end
+   new_array
 end
