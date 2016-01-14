b0a6230c313cb9d47237fc27d5bf7a095ed5e3a5
Sarcasmateur <harrison.lavin@gmail.com>
2016-01-14 14:21:20 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ i=0
+ truth_list=[]
+ while i< collection.length
+  if (yield collection[i]) == true
+    truth_list.push(collection[i])
+  end
+  i+=1
+end
+truth_list
 end
