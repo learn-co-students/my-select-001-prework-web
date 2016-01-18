07355f41795c317da4a8f19d8c760d72d9d241df
akhinchey <akhinchey@gmail.com>
2016-01-18 18:01:48 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+ select_array =[]
+ i = 0
+ if collection.size > 0
+   while i < collection.size
+     if yield(collection[i]) == true
+      select_array << collection[i]
+    end
+    i += 1
+    end
+    select_array
+  else
+    "this collection is empty"
+  end
 end
