6777a19f6b9f7c97b6464a53201c34520aa5e722
chrism81 <chrism81@msn.com>
2016-01-22 11:26:12 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
+ if collection.empty? == false
+  i=0
+  j=[]
+  while i<collection.length
+    if (yield (collection[i])) == true
+      j << collection[i]
+    end
+    i+=1
+  end
+  j
+ end
  # your code here!
 end
