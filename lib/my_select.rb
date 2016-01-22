d33b35fec2bf07b3c2c201c8713deb556856ac79
kunq <Kun_Qian@msn.com>
2016-01-22 12:02:17 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,4 @@
 def my_select(collection)
-  i = 0
-  array_new = []
-  while i < collection.length
-    (array_new << collection[i]) if yield(collection[i]) 
-    i = i + 1
-  end
-  array_new
+  collection.select{ |n| n.even? }
 end
 
