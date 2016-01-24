7a775cf649bee6e3f369317649c56624c01a0277
ericajayne <ericajayne.walsh@gmail.com>
2016-01-24 18:39:07 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
+collection = []
+
 def my_select(collection)
- # your code here!
+ new_collection = []
+ i = 0
+while i < collection.length
+  if yield(collection[i])
+    new_collection<<collection[i]
+    i  = i + 1
+  else i = i + 1
+  end
+  end
+  new_collection
+end
+
+
+my_select(collection) do |n|
+  n.even?
 end
