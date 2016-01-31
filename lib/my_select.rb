e2bfb973e669969ff15286d5335e7cafe7482512
namimi928 <soyongnam@gmail.com>
2016-01-31 17:47:52 -0500

    no message

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+i = 0
+array =[]
+ while i < collection.length
+   yield collection[i]
+  if collection[i]%2 == 0
+    array << collection[i]
+  end
+   i += 1
+ end
+return array
 end
