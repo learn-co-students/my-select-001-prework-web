7267a5d72f15011d67e621fcbb70005e722baefc
Brian Bier <briansbier@gmail.com>
2016-02-06 11:02:33 -0500

    Solution complete

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+i = 0
+new_collection = Array.new
+  while i < collection.length
+    if yield collection[i]
+        new_collection << collection[i]
+    end    
+    i += 1
+  end
+  p new_collection
 end
+
+nums = [1, 2, 3, 4, 5]
+my_select(nums) do |num|
+      num.even?
+    end
