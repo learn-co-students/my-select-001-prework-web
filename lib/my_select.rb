765f957c53def0bee2954463e275179e4805da6e
Cliff <cregisexcel@yahoo.com>
2016-02-17 18:47:17 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
- # your code here!
+   x =0
+   new_arry = []
+  unless collection.empty?
+    while x < collection.count
+        if yield(collection[x])
+          new_arry << collection[x]
+        end
+       x+=1
+     end
+   #collection
+  new_arry
+  end
 end
+
+#     What is the return value of yielding a character to a block?
+#     How can you ensure your .select method can respond appropriately to being passed an argument of an empty collection?
