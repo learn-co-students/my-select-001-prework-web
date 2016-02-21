39d23e1df8b1636e0bb9157f6c1f232fc65687a4
blochbr <blochbr@gmail.com>
2016-02-21 04:20:30 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,8 @@
 def my_select(collection)
- # your code here!
+  counter = 0
+  new_array = []
+  while counter != collection[counter]
+    new_array = collection[counter].even?
+    counter += 1
+  end
 end
