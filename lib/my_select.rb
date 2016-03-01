30a0740011225766e42fd371ed7ad330d2b63829
blochbr <blochbr@gmail.com>
2016-03-01 14:16:17 +0000

    nitrous-backup at Tue Mar  1 14:16:16 UTC 2016

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,9 @@
 def my_select(collection)
   counter = 0
   new_array = []
-  while counter != collection[counter]
-    new_array = collection[counter].even?
+  while counter != collection.length
+    yield (new_array[counter] = collection[counter])
     counter += 1
   end
+  new_array
 end
