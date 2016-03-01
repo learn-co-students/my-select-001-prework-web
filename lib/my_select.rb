07745b1ae2b395f7a646a28af7420b16f95db232
jwrb27 <jwrb27@gmail.com>
2016-02-29 22:16:26 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
+ if collection.empty? == false
+    i = 0
+    new_collection = []
+    while i < collection.length
+      if yield(collection[i])
+        new_collection << collection[i]
+      end
+      i += 1
+    end
+    new_collection
+  else
+    puts "This block should not run!"
+  end
 end
