314bce1780bf34112280e80ece0c2b8cd5f452b4
JennyShalai <jenny.shalai@gmail.com>
2016-03-06 10:24:56 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ 
+ i = 0
+ tempArray = []
+ while i < collection.length
+  elem = yield collection[i]
+  if elem == true
+    tempArray.push(collection[i])
+  end
+    i += 1
+ end
+ tempArray
 end
