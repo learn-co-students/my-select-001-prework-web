1758d713a8ab6bda6bcce3ad53f9a5331948b05e
abillig <abermania@gmail.com>
2016-03-10 13:50:16 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
+x=0
+reminder = []
+while x < collection.length
+  gosh = yield collection[x]
+  if gosh == true 
+reminder << collection[x]
+end 
+  x+=1
+end
+reminder
+
  # your code here!
 end
