ff13ca16e0784a742cf7e29c8b9018e81182455e
Reed Dunkle <reeddunkle@gmail.com>
2016-03-13 18:47:57 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ if !(collection.empty?)
+   i = 0
+   selection = []
+   while i < collection.size
+     if yield(collection[i])
+       selection << collection[i]
+     end
+     i += 1
+   end
+   selection
+ end
 end
