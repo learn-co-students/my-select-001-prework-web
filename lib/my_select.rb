28c52dc5bb988a22cf7e8d857179a69ad47160ca
miggyteez <mjtavarez@yahoo.com>
2016-03-18 13:08:13 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,26 @@
+# collection = [1, 2, 3, 4, 5]
+
 def my_select(collection)
- # your code here!
+  selected = []
+  i = 0
+
+  if collection.length == 0
+    return
+  end
+
+  while i < collection.length
+    x = yield collection[i]
+    if x == true
+    # return where collection[i] was true into selected
+    selected << collection[i]
+  else
+  end
+    i += 1
+  end
+  selected
 end
+
+
+# my_select(collection) do |nums|
+#   nums.even?
+# end
