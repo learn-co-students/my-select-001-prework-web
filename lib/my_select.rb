626379466d0f73467759bf12699dafe641dfadca
TiciaChance <laticia.chance@gmail.com>
2016-03-27 10:08:19 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
-end
+
+def my_select(numbers)
+  even_numbers = []
+  i = 0 
+  while i < numbers.length
+   if yield(numbers[i])
+    even_numbers << numbers[i]
+    end
+    i = i + 1
+  end
+    even_numbers
+  end
