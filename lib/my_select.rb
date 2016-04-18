c07c888286cbbea4e414fdc76bdb0734bd8f4797
WigWog <andyzhao.zhao@gmail.com>
2016-04-18 03:02:41 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+i = 0
+  new_array = []
+  while i < array.length
+    new_element = yield(array[i])
+    new_array << array[i] if new_element == true
+    i += 1
+  end
+  new_array
 end
