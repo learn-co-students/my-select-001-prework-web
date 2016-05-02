7b532d94bfc387ee230e3927591eae3be6f34fdc
p1xt <kjphpdev@gmail.com>
2016-05-02 13:02:49 +0000

    implemented my_select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  output = []
+  i = 0
+  while i < collection.size
+    is_selected = yield collection[i]
+    if is_selected == true
+      output << collection[i]
+    end
+    i += 1
+  end
+  output
 end
