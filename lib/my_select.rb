3c4797ff6e774a1a658446806b0213883e4e446b
iliass sabillah <iliass sabillah>
2016-06-02 19:48:43 -0400

    my_select method

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+    count=0
+    if block_given?
+      out = []
+      while count < array.length
+        out << array[count] if yield(array[count])
+        count+=1
+      end
+      out
+    else
+      array
+    end
 end
