2f51d552ed2680cfae318a4d49fb8c32bf951a02
debashish2549 <debashish.deep74@qmail.cuny.edu>
2016-06-06 02:29:54 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+   i=0
+   new_collection = []
+   while (i<collection.length) do
+      new_collection << (collection[i]) if (yield (collection[i])) == true
+     i+=1
+    end
+    new_collection
+end
+
+
+collection = [1,2,3,4,5,6]
+my_select (collection) do |num|
+  num
 end
