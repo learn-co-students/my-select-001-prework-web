8bc6afb2e8dcf69fcbac5e3a6f9a1df53231d088
Ahmet Calis <calisahmet@gmail.com>
2016-06-08 16:35:22 -0400

    my select first

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
  # your code here!
+ arr = []
+ i = 0
+
+ while i < collection.length
+    arr << collection[i] if yield(collection[i]) == true
+    i += 1
+  end
+  arr 
 end
