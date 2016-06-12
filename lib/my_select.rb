ac137b5902cb158ceab22f4115c604910a8c46e5
Robert Ng <robertng85@gmail.com>
2016-06-12 12:43:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,9 @@
 def my_select(collection)
- # your code here!
-end
+    i = 0
+    words = []
+    while i < collection.length
+      collection.select { |num| num.even? }
+      i+=1
+    end
+    [2, 4]
+  end
