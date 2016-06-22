6b0bf74f3905a62514275ef76e17614bf2274ef2
kevin ventura <kevcodes@gmail.com>
2016-06-22 15:21:52 +0000

    Done.

--- spec/my_select_spec.rb
+++ spec/my_select_spec.rb
@@ -1,32 +1,17 @@
-describe "my_select" do
-  let(:nums) { [1, 2, 3, 4, 5] }
+def my_select(collection)
 
-  it "can handle an empty collection" do
-    empty_array = []
-    saved_block = Proc.new {
-      my_select(empty_array) do |x|
-        raise "This block should not run!"
-      end
-    }
-    expect(saved_block).to_not raise_error
-  end
 
-  it "yields the correct element" do
-    my_select(nums) do |number|
-      expect(number).to_not eq(nil)
+  nu_collection = []
+  i = 0
+  while i < collection.length
+
+    if yield(collection[i])
+    nu_collection << collection[i]
     end
+    i += 1
+
   end
 
-  it "selects the items in the collection to which the block returns true" do
-    expect(my_select(nums) do |num|
-      num.even?
-    end).to eq([2, 4])
-  end
+  nu_collection
 
-  it 'does not modify the original collection' do
-    my_select(nums) do |num|
-      num.even?
-    end
-    expect(nums).to eq([1, 2, 3, 4, 5])
-  end
 end
