02e735e6e8970b83e243e11ffd7832e91524c919
ivanjanko <jankoemail@gmail.com>
2016-06-23 12:46:23 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(coll)
+ i = 0
+ sel_coll = []
+ while i < coll.length
+   yield(coll[i])
+    if coll[i].even?
+      sel_coll << coll[i]
+    end
+    i += 1
+ end
+ sel_coll
 end
