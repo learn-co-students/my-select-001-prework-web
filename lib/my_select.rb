c079bdd7c7e5d4fe7fdb5a166fcf479154e06326
Daniel Toma <dontcallmeshirley80@gmail.com>
2016-06-25 16:41:16 -0400

    my select

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
+def my_select(array)
  # your code here!
+
+  if block_given?
+    new_array = array.select {|item| item % 2 == 0}
+    i = 0
+   # while i < array.length
+    #  new_array << yield(array[i])
+     # i = i + 1
+    #end
+    new_array
+  else
+    puts "This block should not run!"
+  end
 end
+
