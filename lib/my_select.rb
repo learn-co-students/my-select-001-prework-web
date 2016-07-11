2884e3620293ad9573b522fe5c83f8cc514938a7
Melanie Gershman <melanie.gershman@gmail.com>
2016-07-11 18:30:47 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  new_selection = []
+  i = 0
+  while i < collection.length
+	  if yield(collection[i])
+	   	new_selection << collection[i]
+	  end
+	  i += 1
+    end
+  new_selection #return the modified array
 end
