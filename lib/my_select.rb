45c121fcbbf896eed1a864a75848768b6b115389
Colby Gatte <colbygatte@gmail.com>
2016-07-14 13:04:34 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ length = collection.length
+ i = 0
+ n_arr = Array.new
+ while i < length
+   if yield(collection[i])
+     n_arr << collection[i]
+   end
+   i += 1
+ end
+
+ n_arr
 end
