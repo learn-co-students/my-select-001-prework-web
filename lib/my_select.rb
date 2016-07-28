ce32dc2de366ca88f722ef2247de958e7f204839
Steven Tang <5teven7ang@gmail.com>
2016-07-28 03:23:46 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def my_select(collection)
  # your code here!
+ i = 0
+ j = 0
+ selected_items_indices = Array.new
+ selected_items_array = Array.new
+ while i < collection.length do
+   if yield collection[i]
+     selected_items_indices << i
+   end
+   i += 1
+ end
+
+ while j < selected_items_indices.length do
+   if selected_items_indices.length == 0
+     break
+   else selected_items_array << collection[selected_items_indices[j]]
+   end
+   j += 1
+ end
+   selected_items_array
 end
