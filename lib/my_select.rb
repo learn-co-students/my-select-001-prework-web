dba1403abea9ae5f4c20c2351794388ee267ae24
Michael Wiggins <michael.wiggins@me.com>
2016-08-20 18:36:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
-def my_select(collection)
-emptyArray = []
-collection.select { |nums|  nums.even? }#=> [2, 4]
-end
-#Stepping through: You defined a method that takes an argument "collection"
-#it uses .select to select and return the elements, not the index, but the elements |nums| that are even
-#it does not modify the original array, because .select does not transform
-#select { |obj| block } → array
-#select → an_enumerator
-#Returns an array containing all elements of enum for which the given block returns a true value.
-#If no block is given, an Enumerator is returned instead.
+def my_select(array)
+    counter = 0
+    myNewArray = []
+    while counter < array.length
+      if yield(array[counter])
+        myNewArray.push(array[counter])
+    end
+    counter = counter.next
+  end
+  myNewArray
+  end
